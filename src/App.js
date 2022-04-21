b3c0d5fc5858836aa9185fef7dedaf13253df3c9
Mansur Beştaş <mansur@siirt.edu.tr>
2022-04-22 01:05:31 +0300

    .

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import "regenerator-runtime/runtime";
+import React, { useEffect, useState } from "react";
+import { login, logout } from "./utils";
+
+// React Bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// React Bootstraps imports
+import { Nav, Navbar, Container, Row, Card, Alert } from "react-bootstrap";
+
+// Custom Components
+import MintingTool from "./Components/MintingTool";
+import InfoBubble from "./Components/InfoBubble";
+
+// assets
+import Logo from "./assets/logo-white.svg";
+
+import getConfig from "./config";
+const { networkId } = getConfig(process.env.NODE_ENV || "development");
+
+export default function App() {
+  const [userHasNFT, setuserHasNFT] = useState(false);
+ 
+  useEffect(() => {
+    const receivedNFT = async () => {
+      console.log(
+        await window.contract.check_token({
+          id: `${window.accountId}-near-challenge`,
+        })
+      );
+      if (window.accountId !== "") {
+        console.log(
+          await window.contract.check_token({
+            id: `${window.accountId}-near-challenge`,
+          })
+        );
+
+        setuserHasNFT(
+          await window.contract.check_token({
+            id: `${window.accountId}-near-challenge`,
+          })
+        );
+      }
+    };
+    receivedNFT();
+  }, []);
+
+  return (
+    <React.Fragment>
+      {" "}
+      <Navbar bg='dark' variant='dark'>
+        <Container>
+          <Navbar.Brand href='#home'>
+            <img
+              alt=''
+              src={Logo}
+              width='30'
+              height='30'
+              className='d-inline-block align-top'
+            />{" "}
+            NEar SpringField challenge
+            </Navbar.Brand>
+          <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+          <Navbar.Collapse id='responsive-navbar-nav'>
+            <Nav className='me-auto'></Nav>
+            <Nav>
+              <Nav.Link
+                onClick={window.walletConnection.isSignedIn() ? logout : login}
+              >
+                {window.walletConnection.isSignedIn()
+                  ? window.accountId
+                  : "Login"}
+              </Nav.Link>{" "}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container style={{ marginTop: "3vh" }}>
+        {" "}
+        <Row className='d-flex justify-content-center'>
+
+          <img src="https://bafybeiaqt532u7l6dz5acvdd3ekqklisis7rpjfahbzqwsgsbd5inlvewq.ipfs.nftstorage.link/" style={{'height':'350px','width':'300px','justifyContent': 'center',
+    'alignItems': 'center',}}></img>
+        </Row>
+        <br/>
+        <Row>
+          <InfoBubble />
+        </Row>
+        <Row style={{ marginTop: "3vh" }}>
+          <MintingTool userNFTStatus={userHasNFT} />
+        </Row>
+      </Container>
+    </React.Fragment>
+  );
+}
---
+++ src/Components/MintingTool.js
@@ -0,0 +1,65 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Button, Card, Container, Row, Alert } from "react-bootstrap";
+import { keys } from "regenerator-runtime";
+const BN = require("bn.js");
+
+const MintingTool = (props) => {
+  const mintNFT = async () => {
+    await window.contract.nft_mint(
+      {
+        token_id: `${window.accountId}-near-challenge`,
+        metadata: {
+          title: "Near Spring Welcome Nft",
+          description: "Nft only For near SpringField Users",
+          media:
+            "https://bafybeiaqt532u7l6dz5acvdd3ekqklisis7rpjfahbzqwsgsbd5inlvewq.ipfs.nftstorage.link/",
+        },
+        receiver_id: window.accountId,
+      },
+      300000000000000, // attached GAS (optional)
+      new BN("1000000000000000000000000")
+    );
+  };
+
+  return (
+    <Card style={{ padding: "2vh" }}>
+      <Container>
+        <Row style={{ marginBottom: "2vh" }}>
+          <p>
+            Step 2: After you have logged in, hit this button to mint your Token and go your{" "}
+            <a href='https://wallet.testnet.near.org/'> wallet</a> and see your
+            NFT
+          </p>
+        </Row>
+        <Row className='d-flex justify-content-center'>
+          <Button
+            disabled={props.userNFTStatus || window.accountId === ""}
+            onClick={mintNFT}
+            style={{ width: "50vw" }}
+          >
+            Mint NFT
+          </Button>
+        </Row>
+        <Row className='d-flex justify-content-center'>
+          {console.log(props.userNFTStatus)}
+          {props.userNFTStatus ? (
+            <Alert variant='danger' style={{ marginTop: "2vh" }}>
+              <p style={{ textAlign: "center" }}>
+                bruh/sis.... You have an NFT already. You can see it{" "}
+                <a href={"https://wallet.testnet.near.org/?tab=collectibles"}>
+                  here!
+                </a>
+                :)
+              </p>
+            </Alert>
+          ) : null}
+        </Row>
+      </Container>
+    </Card>
+  );
+};
+
+MintingTool.propTypes = {};
+
+export default MintingTool;
